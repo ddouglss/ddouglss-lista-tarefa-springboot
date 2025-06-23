8b6426481a04b1b115d4b45e4b66b27a97c6fb06
ddouglss <ddouglss1999@gmail.com>
2025-06-23 17:20:52 -0300

    feat: inicil de um sistema lista de tarefa com springboot

---
+++ src/main/java/lista/tarefa/springboot_lista_tarefa/SpringbootListaTarefaApplication.java
@@ -0,0 +1,13 @@
+package lista.tarefa.springboot_lista_tarefa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootListaTarefaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootListaTarefaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lista/tarefa/springboot_lista_tarefa/SpringbootListaTarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package lista.tarefa.springboot_lista_tarefa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootListaTarefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
